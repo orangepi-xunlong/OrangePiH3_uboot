2ed572af24abb091b7a0577953cb16101f6c2ba7
cbbc <cbbc_curry@aliyun.com>
2017-01-18 10:46:54 +0800

    [recent add]

---
+++ include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2016.09-g90fb594-dirty"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "arm-linux-gnueabi-gcc (Ubuntu/Linaro 4.7.3-12ubuntu1) 4.7.3"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils for Ubuntu) 2.24"
