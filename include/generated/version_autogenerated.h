909b140ce0ec85a7db22d814735af0dd73e2b61b
cbbc <cbbc_curry@aliyun.com>
2017-01-19 14:14:08 +0800

    [add recent uboot]

--- include/generated/version_autogenerated.h
+++ include/generated/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2016.09-g400cedd-dirty"
+#define PLAIN_VERSION "2016.09-gf9e6c78-dirty"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CC_VERSION_STRING "arm-linux-gnueabi-gcc (Ubuntu/Linaro 4.7.3-12ubuntu1) 4.7.3"
 #define LD_VERSION_STRING "GNU ld (GNU Binutils for Ubuntu) 2.24"
